d1e16f9f5908891512684a484f5e0de99f9e4a80
Kevin Morrell <kmorrell827@gmail.com>
2023-04-26 15:29:04 -0700

    Moving to Unity?

---
+++ Unity/Boots/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public float speed;
+  private Rigidbody2D myRigidbody;
+  private Vector2 change;
+  private Animator animator;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    animator = GetComponent<Animator>();
+    myRigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    change = Vector2.zero;
+    change.x = Input.GetAxisRaw("Horizontal");
+    change.y = Input.GetAxisRaw("Vertical");
+    UpdateAnimationAndMove();
+  }
+
+  void MoveCharacter()
+  {
+    Vector2 position = transform.position;
+    myRigidbody.MovePosition(position + change * speed * Time.deltaTime);
+  }
+
+  void UpdateAnimationAndMove()
+  {
+    if (change != Vector2.zero)
+    {
+      MoveCharacter();
+      animator.SetFloat("moveX", change.x);
+      animator.SetFloat("moveY", change.y);
+      animator.SetBool("moving", true);
+    }
+    else
+    {
+      animator.SetBool("moving", false);
+    }
+  }
+}
