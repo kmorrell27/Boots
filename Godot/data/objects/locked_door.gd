232fb226ead2d81e7e7e614177dc79981145464a
Kevin Morrell <kmorrell827@gmail.com>
2024-08-16 21:45:41 -0700

    Updated to 4.3!
    Also I added a neat little animation for the doors.

--- Godot/data/objects/locked_door.gd
+++ Godot/data/objects/locked_door.gd
@@ -1,6 +1,11 @@
 class_name LockedDoor extends StaticBody2D
 
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
 func maybe_unlock(body: Node2D) -> void:
 	if (body is Player and Inventory.keys > 0):
 		Inventory.keys -= 1
-		queue_free()
+		anim.play("Unlock")
+
+func _on_animation_finish() -> void:
+	queue_free()
